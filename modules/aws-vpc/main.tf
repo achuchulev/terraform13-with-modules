d12eea6dc1251ed5b2d1971497e4678bc55b4ed0
Atanas Chuchulev <atanas.v4@gmail.com>
2020-08-13 16:56:05 +0300

    Update main.tf

--- modules/aws-vpc/main.tf
+++ modules/aws-vpc/main.tf
@@ -1,8 +1,8 @@
 # Configure the AWS Provider
-provider "aws" {
-  version = "~> 3.0"
-  region  = "us-east-1"
-}
+#provider "aws" {
+#  version = "~> 3.0"
+#  region  = "us-east-1"
+#}
 
 # Create a VPC
 resource "aws_vpc" "example" {
