db859245b5901f7547faffe85d296cf95289d8b6
Atanas Chuchulev <atanas.v4@gmail.com>
2020-08-13 16:49:11 +0300

    Create main.tf

---
+++ modules/aws-vpc/main.tf
@@ -0,0 +1,13 @@
+# Configure the AWS Provider
+provider "aws" {
+  version = "~> 3.0"
+  region  = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "18.8.0.0/16"
+  tags = {
+      Name = "atanas-test"
+  }
+}
